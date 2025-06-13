af75d6e18b48b61a05a55f4add0b77f4da7541ee
Parshvi Jain <parshvijain1508@gmail.com>
2025-06-13 23:36:39 +0530

    deployment parcel stuff

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 import './styles.css';
 import './background.js';
+
 document.addEventListener('DOMContentLoaded', () => {
   // Firebase initialization using only environment variables
   const firebaseConfig = {
-    apiKey: process.env.PARCEL_FIREBASE_API_KEY,
-    authDomain: process.env.PARCEL_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.PARCEL_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.PARCEL_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.PARCEL_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.PARCEL_FIREBASE_APP_ID,
-    measurementId: process.env.PARCEL_FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
   };
 
   // Initialize Firebase
